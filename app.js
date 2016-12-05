3715ff4d9c46435688d168400b0e60d548a43f3c
likoshow <likoshow@gmail.com>
2016-12-05 21:06:29 +0800

    change markdown render lib, add a simple css file

--- app.js
+++ app.js
@@ -2,34 +2,45 @@
 var express = require('express');
 var fs = require('fs');
 var marked = require('marked');
+var markdown = require( "markdown" ).markdown;
+
+
 var app = express();
 
+
+var htmlWrap = ''
+try {
+  htmlWrap = fs.readFileSync('./index.html','utf-8');
+} catch (e) {
+  throw e
+  return
+}
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   fs.readdir('./blogs', function (err, files) {
     if (err) res.send(err);
 
-    fs.readFile('./index.html','utf-8', function (err, html) {
-      if (err) res.send(err);
+    var lis = files.map(function (filename) {
+      var file = filename.split('.')[0]
 
-      var lis = files.map(function (filename) {
-        var file = filename.split('.')[0]
-
-        return '<li><a href="/blog/' + file +'">' + file +'</a></li>'
-      })
-
-      var ulHtml = '<ul>' + lis.join('') +'</ul>'
-      res.send(html.replace('REPLACE_STRING', ulHtml))
+      return '<li><a href="/blog/' + file +'">' + file +'</a></li>'
     })
+
+    var ulHtml = '<ul>' + lis.join('') +'</ul>'
+    res.send(htmlWrap.replace('REPLACE_STRING', ulHtml))
   })
 });
 
 app.get('/blog/:title', function (req, res) {
-  console.log(req.params.title);
+
   var title = req.params.title
   fs.readFile('./blogs/' + title + '.md','utf-8', function (err, data) {
     if (err) res.send('Not Found: ' + title);
-    res.send(marked(data));
+
+    res.send(htmlWrap.replace('REPLACE_STRING', markdown.toHTML(data)));
+
   });
   //blog page
 });
@@ -38,4 +49,4 @@ app.get('*', function(req, res) {
 
 });
 app.listen(80);
-console.log('server is running at 3000!');
+console.log('server is running at 80!');
